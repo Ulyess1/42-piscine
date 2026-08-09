d099adf146d49100bbc9f839589e05047e1ceaeb
Ulyess1 <hhj3861@gmail.com>
2026-08-09 01:06:27 +0100

    shell01

---
+++ shell01/ex07/r_dwssap.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat /etc/passwd | grep -v '#' | awk 'NR % 2 == 0' | cut -d ':' -f1 | rev | sort -r | awk -v start="$FT_LINE1" -v end="$FT_LINE2" 'NR >= start && NR <= end' | tr '\n' ',' | sed 's/,$/./' | sed 's/,/, /g'
---
+++ shell01/ex08/add_chelou.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "$FT_NUM1 + $FT_NUM2" \
+| sed "s/'/1/g" \
+| tr '\\\"\?\!' 5678 \
+| tr 'mrdoc' 56789 \
+| xargs echo 'obase=13; ibase=5;' \
+| bc \
+| tr 56789ABCDEF 'gtaio luSnemf'
